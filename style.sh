9bb0799889bd1f28a963f917f7a4a604438c629e
abdalla435 <115881091+abdalla435@users.noreply.github.com>
2023-08-09 01:54:38 +0300

    Create style.sh

---
+++ style.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+BLACK="\e[30m"
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+MAGENTA="\e[35m"
+CYAN="\e[36m"
+WHITE="\e[37m"
+large="\e#3"
+BOLD="\e[1m"
+neofetch
+echo -e "ㅤ"
+echo -e "ㅤ"
+echo -e "ㅤ"
+echo -e "ㅤ"
+echo -e "ㅤ"
+echo -e "ㅤ"
+echo -e "ㅤ"
+
+echo -e "${YELLOW} - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
+echo -e "${BOLD}${GREEN}Powered By"
+echo -e "
+${RED}  _   _____  ________              ________
+${RED} | | / / _ \/ __/ __/______ ___   / __/ __/
+${RED} | |/ / ___/\ \/ _// __/ -_) -_) / _/_\ \  
+${RED} |___/_/  /___/_/ /_/  \__/\__(_)___/___/"
+echo -e "${YELLOW} - - - - - - - - - - - - - - - - ${WHITE}[ ${BOLD}${GREEN}TYPE YOUR COMMAND ${WHITE}]${YELLOW} - - - - - - - - - - - - - - "
